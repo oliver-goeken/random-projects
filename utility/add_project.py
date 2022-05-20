0054595330fec2ea644a6dfd37a7b9c397410659
oliver-goeken <bugseverywhere@protonmail.com>
2022-05-20 00:49:56 -0700

    finish program to create new projects

--- utility/add_project.py
+++ utility/add_project.py
@@ -1,9 +1,23 @@
 import sys
 import os
 
+#allow ANSI escape sequences to work
+from colorama import init
+init()
+
 if(len(sys.argv) == 4):
-    # TODO: Add functionality (os-specific)
-    print()
+    name = sys.argv[1]
+    type = sys.argv[2]
+    folder = sys.argv[3]
+
+    print("creating folder ... ", end = "")
+    os.system(f"mkdir ..\\{folder}\\{name} || mkdir ../{folder}/{name}")
+    print("done.")
+
+    if type == "python":
+        print("creating virtual environment ... ", end = "")
+        os.system(f"python3 -m venv ../{folder}/{name}/venv/ || python3 -m venv ..\\{folder}\\{name}\\venv/")
+        print("done.")
 else:
     print("USAGE: add_project [project name] [project type] (project_folder)")
 
@@ -13,4 +27,4 @@ else:
             print("Project Types:")
             print(" -Python")
     
-    print()
+print()
